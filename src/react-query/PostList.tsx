5b950f739d13144e59d19025ce04f1e8518ba068
IkyIsaac <isaackitomari33@gmail.com>
2025-04-03 02:05:11 +0300

    Implement pagination

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -5,11 +5,16 @@ import { usePosts } from './hooks/usePosts';
 
 
 const PostList = () => {
-  const {data:posts,error,isLoading}=usePosts();
+  const [page,setPage]=useState(1);
+  const pageSize =10;
+  const {data:posts,error,isLoading}=usePosts({page, pageSize});
 
+
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
+    <>
     <ul className="list-group">
       {posts?.map((post) => (
         <li key={post.id} className="list-group-item">
@@ -17,6 +22,10 @@ const PostList = () => {
         </li>
       ))}
     </ul>
+    <button className="btn btn-primary mt-3" disabled={page===1} onClick={()=>setPage(page-1)}>Previous</button>
+    <span className="mx-2 mt-3">{page}</span>
+    <button className="btn btn-primary mt-3" onClick={()=>setPage(page+1)}>Next</button>
+    </>
   );
 };
 
--- src/react-query/hooks/usePosts.tsx
+++ src/react-query/hooks/usePosts.tsx
@@ -7,15 +7,26 @@ interface Post {
     userId: number;
   }
 
-export const usePosts = () => {
+interface PostQuery{
+    page?:number,
+    pageSize?:number,
+}
+
+export const usePosts = (query:PostQuery) => {
     const fetchPosts=()=>axios
-    .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+    .get<Post[]>('https://jsonplaceholder.typicode.com/posts',{
+        params:{
+            _start: ((query.page ?? 1) - 1) * (query.pageSize ?? 10),
+            _limit:query.pageSize
+        }
+    })
     .then(res=>res.data);
 
     return useQuery<Post[],Error>({
-        queryKey:['posts'],
+        queryKey:['posts',query],
         queryFn:fetchPosts,
         staleTime:10*1000,
+        keepPreviousData:true,
     })
 }
 
