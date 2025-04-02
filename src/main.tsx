611e77caabfce468a3a54f47ba61627ce41731db
IkyIsaac <isaackitomari33@gmail.com>
2025-04-03 01:25:21 +0300

    Implement Fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const queryClient = new QueryClient();
+// {
+//   defaultOptions:{
+//     queries:{
+//       retry:3,
+//       staleTime:10*1000,
+//       refetchOnWindowFocus:false,
+//       refetchOnMount:true,
+//       refetchOnReconnect:false,
+//     }
+//   }
+// }
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   </React.StrictMode>
 );
